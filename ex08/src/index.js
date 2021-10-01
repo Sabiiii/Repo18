71a3c851a37d997de3658fdfbddc5fb6f188d2b6
Sabiiii <sabinaajanovic27@gmail.com>
2021-10-01 10:46:47 +0400

    Adding maping and filtering

---
+++ ex08/src/index.js
@@ -0,0 +1,13 @@
+let one = new Set([1, 2, 3, 4, 5]);
+let two = new Set([6, 4, 3, 2]);
+let unionSet = new Set([...one, ...two]);
+let intersectionSet = new Set([...one].filter((i) => two.has(i)));
+let diffeerenceSet = new Set([...one].filter((i) => !two.has(i)));
+
+console.log(one);
+console.log(two);
+console.log(unionSet);
+console.log(intersectionSet);
+console.log(diffeerenceSet);
+module.exports = { unionSet, intersectionSet, diffeerenceSet };
+
